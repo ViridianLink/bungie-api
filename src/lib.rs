eca332b38b9b627c539a5723d8532ba49bf7103a
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-11-10 20:39:22 +0000

    Add type aliases for Destiny inventory and socket definitions in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod bungie_client;
 
 pub mod endpoints;
 mod error;
+use std::collections::HashMap;
+
 pub use error::Error;
 use error::Result;
 
@@ -12,6 +14,11 @@ pub use types::definitions::DestinyInventoryItemDefinition;
 pub use types::destiny::definitions::sockets::{
     DestinyPlugSetDefinition, DestinySocketCategoryDefinition, DestinySocketTypeDefinition,
 };
+
+pub type DestinyInventoryItemManifest = HashMap<String, DestinyInventoryItemDefinition>;
+pub type DestinyPlugSetManifest = HashMap<String, DestinyPlugSetDefinition>;
+pub type DestinySocketCategoryManifest = HashMap<String, DestinySocketCategoryDefinition>;
+pub type DestinySocketTypeManifest = HashMap<String, DestinySocketTypeDefinition>;
 
 #[cfg(test)]
 mod tests {
