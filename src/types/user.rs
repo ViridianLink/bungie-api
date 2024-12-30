753bb1c96f6d3da4de8de1da1061b6cd8535f092
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-12-30 21:10:07 +0000

    Make display_name field optional in UserInfoCard struct

--- src/types/user.rs
+++ src/types/user.rs
@@ -16,7 +16,7 @@ pub struct UserInfoCard {
     pub membership_type: BungieMembershipType,
     #[serde(deserialize_with = "string_to_u64")]
     pub membership_id: u64,
-    pub display_name: String,
+    pub display_name: Option<String>,
     pub bungie_global_display_name: String,
     pub bungie_global_display_name_code: i16,
 }
